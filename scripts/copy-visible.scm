84509a89e7a55c01fad8d01d7fb17cc6cb50a1d1
Kevin Cozens <kcozens@cvs.gnome.org>
2006-05-26 22:12:37 +0000

    Use canonical procedure names in the help string. From a change made by
    2006-05-26  Kevin Cozens  <kcozens@cvs.gnome.org>
    
    	* scripts/copy-visible.sct: Use canonical procedure names in the
    	help string. From a change made by Michael Natterer to Script-Fu.

--- scripts/copy-visible.scm
+++ scripts/copy-visible.scm
@@ -1,19 +1,19 @@
 ; The GIMP -- an image manipulation program
 ; Copyright (C) 1995 Spencer Kimball and Peter Mattis
-; 
+;
 ; "Copy Visible" -- copy the visible selection so that it can be pasted easily
 ; Copyright (C) 2004 Raphaël Quinet, Adrian Likins, Sven Neumann
 ;
 ; This program is free software; you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
 ; the Free Software Foundation; either version 2 of the License, or
-; (at your option) any later version.  
-; 
+; (at your option) any later version.
+;
 ; This program is distributed in the hope that it will be useful,
 ; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ; GNU General Public License for more details.
-; 
+;
 ; You should have received a copy of the GNU General Public License
 ; along with this program; if not, write to the Free Software
 ; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -32,7 +32,7 @@
 ; Removed all code and made it a backward-compat wrapper around
 ;     (gimp-edit-copy-visible)
 ;     2004-12-12 Michael Natterer <mitch@gimp.org>
-; 
+;
 
 (define (tiny-fu-copy-visible image drawable)
   (gimp-edit-copy-visible image)
@@ -40,7 +40,7 @@
 
 (tiny-fu-register "tiny-fu-copy-visible"
     "Copy Visible"
-    "This procedure is deprecated! use \'gimp_edit_copy_visible\' instead."
+    "This procedure is deprecated! use \'gimp-edit-copy-visible\' instead."
     ""
     ""
     ""
