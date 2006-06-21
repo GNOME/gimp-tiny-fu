433fe0d70be5e0dbc4b8a56a6aa8a1233ae233c9
Kevin Cozens <kcozens@cvs.gnome.org>
2006-06-21 15:10:14 +0000

    Oops...this file didn't get committed as part of the changes that fixed
    2006-06-21  Kevin Cozens  <kcozens@cvs.gnome.org>
    
    	* tiny-fu/tiny-fu-interface.h: Oops...this file didn't get committed
    	as part of the changes that fixed bug #330981.

--- tiny-fu/tiny-fu-interface.h
+++ tiny-fu/tiny-fu-interface.h
@@ -19,9 +19,8 @@
 #ifndef __TINY_FU_INTERFACE_H__
 #define __TINY_FU_INTERFACE_H__
 
-gboolean tiny_fu_interface_is_dialog_open (void);
-
-void  tiny_fu_interface           (SFScript *script);
-void  tiny_fu_interface_report_cc (gchar    *command);
+void       tiny_fu_interface                (SFScript    *script);
+void       tiny_fu_interface_report_cc      (const gchar *command);
+gboolean   tiny_fu_interface_is_dialog_open (void);
 
 #endif /*  __TINY_FU_INTERFACE__  */
